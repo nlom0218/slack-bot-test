f1d81a237bd30a4fe6f9f7fb7e2b13cfcc593313
nlom0218 <nlom0218@naver.com>
2023-05-02 17:10:51 +0900

    feat: 메시지 보내기

--- src/App.js
+++ src/App.js
@@ -1,23 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+import axios from 'axios';
+import { useState } from 'react';
 
 function App() {
+  const [message, setMessage] = useState('');
+
+  const handleChangeMessage = (event) => {
+    setMessage(event.target.value);
+  };
+
+  const handleSubmitMessage = async () => {
+    try {
+      const { data } = axios({
+        method: 'post',
+        url: 'https://hooks.slack.com/services/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',
+        headers: {
+          'Content-Type': 'application.json',
+        },
+        data: {
+          blocks: [
+            {
+              type: 'section',
+              text: {
+                type: 'mrkdwn',
+                message,
+              },
+            },
+          ],
+        },
+      });
+      return data;
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <input
+        value={message}
+        onChange={handleChangeMessage}
+        type="text"
+        placeholder="메시지 내용을 입력"
+      />
+      <button type="button" onClick={handleSubmitMessage}>
+        전송
+      </button>
     </div>
   );
 }
